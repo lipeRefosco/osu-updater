b1c9563c60b6a9b9b91e0f045b7aa41ecdeca0be
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-10-18 16:37:10 -0300

    test: adding more tests for the messages

--- src/messages_test.py
+++ src/messages_test.py
@@ -1,8 +1,22 @@
-import pytest
 from messages import MESSAGES
 from version_handler import send_notification
 
 class TestMessages:
+
+    def test_message_have_update(self):
+        exit_code = send_notification(MESSAGES['have_update'])
+        assert exit_code == 0
+
+
+    def test_message_downloading(self):
+        exit_code = send_notification(MESSAGES['downloading'])
+        assert exit_code == 0
+
+
+    def test_message_instalation_finished(self):
+        exit_code = send_notification(MESSAGES['instalation_finished'])
+        assert exit_code == 0
+
 
     def test_message_fail_install(self):
         exit_code = send_notification(MESSAGES['fails']['install'])

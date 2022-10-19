b34240e8332fd2987d76a93553adc14bdfec4cd5
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-10-19 11:21:05 -0300

    feat: definition of .desktop file

---
+++ src/desktopfile.py
@@ -0,0 +1,9 @@
+dektop_file = """
+[Desktop Entry]
+Type=Application
+Name=Osu Lazer
+Exec=/home/lipe/Games/osuLazer/osu.AppImage
+Terminal=false
+Icon=/home/lipe/Games/osuLazer/osulazer.png
+Categories=Games;
+"""

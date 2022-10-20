b012c2b5be0c35698abeacda5324ff1f45c268a8
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-10-20 00:40:21 -0300

    fix: add command to run the game

--- src/desktopfile.py
+++ src/desktopfile.py
@@ -2,7 +2,7 @@ desktop_file = """
 [Desktop Entry]
 Type=Application
 Name=Osu Lazer
-Exec=/home/lipe/Games/osuLazer/osu.AppImage
+Exec=python /home/lipe/Games/osuLazer/osu.AppImage
 Terminal=false
 Icon=/home/lipe/Games/osuLazer/osulazer.png
 Categories=Games;
